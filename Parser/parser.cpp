3e4bb1d063aa65a8958484c3acd5772140ca2832
Misash <misash@outlook.com>
2022-01-27 11:12:29 -0500

    start parser

---
+++ Parser/parser.cpp
@@ -0,0 +1,34 @@
+//
+// Created by misash on 27/01/22.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+class Parser{
+public:
+
+    int a = 0;
+
+    Parser(){
+
+    }
+
+    void E(){
+        cout<<"asd";
+    }
+
+
+};
+
+
+
+int main(){
+
+
+
+
+
+}
